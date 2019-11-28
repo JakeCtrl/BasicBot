2bade2560e2e6959a664f2d5ad9bf6a138e33ff5
JakeCtrl <jakeinctrl@gmail.com>
2019-11-28 01:58:09 -0500

    1

--- src/main/java/Bot.java
+++ src/main/java/Bot.java
@@ -3,13 +3,20 @@ import commands.RemindMe;
 import events.RkoEvent;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
+import java.util.Scanner;
+import java.io.File;
 
 import javax.security.auth.login.LoginException;
 
 public class Bot {
     public static void main(String[] argv) throws Exception {
 
-        JDA jda = new JDABuilder("NTEyNDcyODM4NzQ2Mjc1ODcx.Xd2bLQ.twBWNT1UqEnie2WaT20fmgiI-QA").build();
+        File file = new File("C:\\Users\\Ctrl\\IdeaProjects\\BasicBot\\src\\main\\java\\token.txt");
+        Scanner input = new Scanner(file);
+        String token = input.nextLine();
+
+
+        JDA jda = new JDABuilder(token).build();
 
         jda.addEventListener(new RkoEvent());
         jda.addEventListener(new RemindMe());
