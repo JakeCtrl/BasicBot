83f8639d9bcdc3f00930fe3ac38ce01bf64ce580
JakeCtrl <jakeinctrl@gmail.com>
2019-11-29 02:55:45 -0500

    1

--- src/main/java/commands/ServerInfo.java
+++ src/main/java/commands/ServerInfo.java
@@ -10,7 +10,7 @@ import java.util.Arrays;
 public class ServerInfo extends Command {
 
     public ServerInfo(){
-        this.name = "serverinfo";
+        this.name = "ServerInfo";
         this.aliases = new String[]{"server"};
         this.help = "Gives information about the server.";
 
@@ -25,12 +25,12 @@ public class ServerInfo extends Command {
         }
 
         EmbedBuilder eb = new EmbedBuilder();
-        eb.setColor(Color.GREEN);
+        eb.setColor(Color.RED);
         eb.setAuthor(e.getGuild().getName());
-        eb.setThumbnail("https://www.google.com/url?sa=i&url=https%3A%2F%2Fstore.hp.com%2Fus%2Fen%2Ftech-takes%2Fhow-to-overclock-pc-cpu&psig=AOvVaw0Qkqg6J0VKpMDjn9Y1F3gx&ust=1575094477328000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCMDax6jijuYCFQAAAAAdAAAAABAE");
+        eb.setThumbnail("http://img3.wikia.nocookie.net/__cb20121223024203/callofduty/images/f/f1/Zombie_Rank_8_Icon_BOII.png");
         eb.addField("Server Owner: " , e.getGuild().getOwner().getEffectiveName(), true);
-        eb.addField("MemberCount: ", Integer.toString(e.getGuild().getMembers().size()), true);
-        eb.setDescription("**Members: \n " + Arrays.toString(members) + "\n **Invite Link** \n" + "");
+        //eb.addField("MemberCount: ", Integer.toString(e.getGuild().getMembers().size()), true);
+        eb.setDescription("**Invite Link** \n" + "");
 
         //e.reply("This is a test");
         e.getChannel().sendMessage(eb.build()).queue();
